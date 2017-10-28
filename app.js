15ae4d3cf5bb7b98a05dfc62a6c719662f7d1ddd
Ronak Shah <ronakshah2013@u.northwestern.edu>
2017-10-28 14:38:56 -0500

    Google CORS

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
-function myFunction(){
-    $.ajax({
-        url: 'https://api.yelp.com/v3/businesses/',
-    dataType: 'JSON',
-    beforeSend: function(xhr) {
-                xhr.setRequestHeader("Authorization", "Bearer $token")
-            },
-    success: function(data){
-        console.log(data)
-    },
-    error: function(jqXHR, textStatus, errorThrown)   {
-                    console.log(textStatus);
-                }
-
-    });
+function googleSucks(){
+    var proxyURL = 'https://cors-anywhere.herokuapp.com/';
+    var baseURL = 'https://maps.googleapis.com/maps/api/place/details/json?key=AIzaSyC3GVP9Gg8dyaCEctUcd5g1JcBdtRkH1c4&placeid=ChIJq61P2zfTD4gR4dBUMWk_fj0'
+    var realURL = baseURL
+    $.getJSON(realURL, function(response){
+        console.log(response);
+    })
 };
-myFunction();
+
+googleSucks();
